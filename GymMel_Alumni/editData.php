f3db1c0e7e1cd5d5bf1d68fb9606e91e80892a68
Jan <96944229+modelrailroader@users.noreply.github.com>
2024-01-01 19:15:41 +0100

    feat: migrate editData to twig

--- GymMel_Alumni/editData.php
+++ GymMel_Alumni/editData.php
@@ -4,10 +4,12 @@ include_once(__DIR__ . '/vendor/autoload.php');
 include_once(__DIR__ . '/src/User.php');
 include_once(__DIR__ . '/src/DataHelper.php');
 include_once(__DIR__ . '/src/Logs.php');
+include_once(__DIR__ . '/src/Template.php');
 
 use src\User;
 use src\DataHelper;
 use src\Logs;
+use src\Template;
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
@@ -60,68 +62,25 @@ if(is_null($submit) & is_null($dataid)) {
 }
 
 include 'header.php';
-?>
 
-            <div class="container mt-5">
-                <?php
-                if(isset($success_message)) {
-                    print($success_message);
-                }
-                ?>
+$template = new Template('./assets/templates');
+$template->setTemplate('editData.twig');
 
-                <h1>Datensatz bearbeiten</h1>
-                <div style="margin-top: 30px"></div>
-                <form action="<?php print($_SERVER['PHP_SELF']) ?>" method="post">
-                    <div class="mb-3">
-                        <label for="name" class="form-label">
-                            <i class="bi bi-person"></i>&nbsp; Vor- und Nachname:
-                        </label>
-                        <input type="text" class="form-control" id="name" name="name" value="<?php print($data['name']) ?>" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="email" class="form-label">
-                            <i class="bi bi-envelope-at"></i>&nbsp; E-Mail-Adresse:
-                        </label>
-                        <input type="email" class="form-control" id="email" name="email" value="<?php print($data['email']) ?>" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="ausbildung" class="form-label">
-                            <i class="bi bi-book"></i>&nbsp; Studium/Ausbildung:
-                        </label>
-                        <input type="text" class="form-control" id="studies" name="studies" value="<?php print($data['studies']) ?>">
-                    </div>
-                    <div class="mb-3">
-                        <label for="beruf" class="form-label">
-                            <i class="bi bi-briefcase"></i>&nbsp; Beruf/Tätigkeit:
-                        </label>
-                        <input type="text" class="form-control" id="job" name="job" value="<?php print($data['job']) ?>">
-                    </div>
-                    <div class="mb-3">
-                        <label for="firma" class="form-label">
-                            <i class="bi bi-building"></i>&nbsp; Aktueller Arbeitgeber:in:
-                        </label>
-                        <input type="text" class="form-control" id="company" name="company" value="<?php print($data['company']) ?>">
-                    </div>
-                    <div class="mb-3">
-                        <label for="registrierungszeitpunkt" class="form-label">
-                            <i class="bi bi-calendar-date"></i>&nbsp; Registrierung
-                        </label>
-                        <input type="text" class="form-control" id="date_registered" name="date_registered" value="<?php print(date('d.m.Y - H:i', $data['date_registered'])) ?>" readonly>
-                    </div>
-                    <div class="mb-3">
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input" id="transfer_privacy" name="transfer_privacy" <?php print($dataHelper->getTransferPrivacyFromData($data)) ?>>
-                            <label class="form-check-label" for="datenschutz">Weitergabe der Daten an Schüler:innen</label>
-                        </div>
-                    </div>
-                    <input type="hidden" id="id" name="id" value="<?php print($dataid) ?>">
-                    <div style="margin-top: 20px"></div>
-                    <button type="submit" id="submit" name="submit" class="btn btn-primary">Speichern</button>
-                    <div style="margin-top: 50px"></div>
-                </form>
-            </div>
+$templateVars = [
+    'php_self' => filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_SPECIAL_CHARS),
+    'name' => $data['name'],
+    'email' => $data['email'],
+    'studies' => $data['studies'],
+    'job' => $data['job'],
+    'company' => $data['company'],
+    'date_registered' => date('d.m.Y - H:i', $data['date_registered']),
+    'transfer_privacy' => $dataHelper->getTransferPrivacyFromData($data),
+    'dataid' => $dataid,
+    'success_message' => $success_message
+];
 
-<?php 
+echo $template->render($templateVars);
+
 include 'footer.php';
 
 
