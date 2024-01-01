2a36822b4ed3e1b06f4ea90940e3ad1e2a202071
Jan <96944229+modelrailroader@users.noreply.github.com>
2024-01-01 18:04:36 +0100

    feat: migrate header to twig

--- GymMel_Alumni/header.php
+++ GymMel_Alumni/header.php
@@ -2,76 +2,21 @@
 include 'constants.php';
 include_once(__DIR__.'/vendor/autoload.php');
 include_once(__DIR__.'/src/User.php');
+include_once(__DIR__.'/src/Template.php');
 
 use src\User;
+use src\Template;
 
 if(!isset($user)) {
     $user = new User();
 }
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="icon" href="favicon.ico" type="image/x-icon">
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+$template = new Template('./assets/templates');
+$template->setTemplate('header.twig');
 
-        <link rel="stylesheet" href="assets/dist/styles.css">
-    </head>
-    <body>
-        <div class="content-wrapper" style="display: flex; flex-direction: column; min-height: 100vh">
-            <header class="bg-primary text-white text-center pb-2 pt-1" id="background-color-1" style="height: auto">
-                <div class="container pt-2">
-                    <div class="row align-items-center">
-                        <div class="col-md-2">
-                            <a href="index.php"><img src="img/logo.png" alt="Gymnasium Melle Logo" class="img-fluid" style="max-width: 150px;"></a>
-                        </div>
-                        <div class="col-md-10">
-                            <nav class="navbar navbar-expand-lg navbar-dark">
-                                <div class="container">
-                                    <button class="navbar-toggler navbar-dark" style="--bs-navbar-color: #f1f2ed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                        <i class="bi bi-list" style="color: #868686; font-size: 30px"></i>
-                                    </button>
-                                    <div class="collapse navbar-collapse" id="navbarNav">
-                                        <ul class="navbar-nav">
-                                            <li class="nav-item">
-                                                <a class="nav-link" style="color: #000" href="index.php">Alumni werden</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link" style="color: #000" href="https://www.melle-gymnasium.de">Website der Schule</a>
-                                            </li>
-                                        </ul>
-                                    <?php
-                                    if ($user->isLoggedIn()) {
-                                        print(
-                                                '<div class="justify-content-end ms-auto">
-                                                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false" style="text-decoration: none; color: black">'
-                                                    . $user->getUsername() . '</a>
-                                                      <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuLink">
-                                                        <li><a class="dropdown-item" href="showData.php">Netzwerkdatenbank</a></li>
-                                                        <li><a class="dropdown-item" href="users.php">Benutzerübersicht</a></li>
-                                                        <li><a class="dropdown-item" href="backup.php">Backup</a></li>
-                                                        <li><a class="dropdown-item" href="Dokumentation.pdf">Dokumentation</a></li>
-                                                        <li class="dropdown-divider"></li>
-                                                        <li><a class="dropdown-item" href="ucp.php">Persönlicher Bereich</a></li>
-                                                        <li><a class="dropdown-item" href="logout.php">Ausloggen</a></li>
-                                            </div>');
-                                    } else {
-                                        print(
-                                                '<ul class="navbar-nav ms-auto">
-                                                    <li class="nav-item">
-                                                    <a class="nav-link" style="color: #000" href="login.php">Einloggen</a>
-                                                    </li>
-                                                </ul>
-                                            </div>');
-                                    }
-                                    ?>
-                                </div>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            </header>
+$templateVars = [
+    'userIsLoggedIn' => $user->isLoggedIn(),
+    'username' => $user->getUsername()
+];
 
+echo $template->render($templateVars);
