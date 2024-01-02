ed8506e6d07fdc8e5c1b0acec265de949681d271
Jan <96944229+modelrailroader@users.noreply.github.com>
2024-01-02 13:19:51 +0100

    feat: migrate twofactor-auth to twig

--- GymMel_Alumni/twofactor_code.php
+++ GymMel_Alumni/twofactor_code.php
@@ -6,9 +6,12 @@ if(session_status() === PHP_SESSION_NONE) {
 include_once(__DIR__.'/vendor/autoload.php');
 include_once(__DIR__.'/src/User.php');
 include_once(__DIR__.'/src/Alert.php');
+include_once(__DIR__.'/src/Template.php');
 
 use src\User;
 use src\Alert;
+use src\Template;
+
 $user = new User();
 $alert = new Alert();
 
@@ -25,36 +28,14 @@ if($error === 1) {
 }
 
 include 'header.php';
-?>
 
-            <div class="container">
-            <?php
-            if(isset($success_message)) {
-                print($success_message);
-            } ?>
-            <div class="container mt-5 justify-content-center align-items-center" style="max-width: 500px; display: flex">
-                <div class="login-form" style="width: 100%; border: 0.5px solid #ccc; border-radius: 10px; padding: 40px">
-                    <h2>2-Faktor-Authentifizierung</h2>
-                    <div style="margin-top: 20px"></div>
-                    <p>Gib den Code aus deiner Authenticator-App ein, um Zugriff zu erhalten.</p>
-                    <div style="margin-top: 20px"></div>
-                    <form action="index.php" method="POST">
-                        <input type="hidden" name="userid" value="<?php print($userid) ?>" id="userid">
-                        <input type="hidden" id="action" value="twofactor" name="action">
-                        <div class="mb-3">
-                            <label for="twofactor_code" class="form-label">
-                                <i class="bi bi-key"></i> Code:
-                            </label>
-                            <input type="text" class="form-control" id="twofactor_code" name="twofactor_code" required>
-                        </div>
-                        <div style="margin-top: 20px;"></div>
-                        <button type="submit" class="btn btn-primary">Anmelden</button>
-                    </form>
-                </div>
-              </div>
-            </div>
+$template = new Template('./assets/templates');
+$template->setTemplate('twofactor_code.twig');
 
-            <div style="margin-top: 50px;"></div>
+$templateVars = [
+    'success_message' => isset($success_message) ? $success_message : ''
+];
 
-<?php
+echo $template->render($templateVars);
+
 include 'footer.php';
