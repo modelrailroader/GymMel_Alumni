3eced672e01ab5f2137e4238f952caf4f4c4fab1
Jan <96944229+modelrailroader@users.noreply.github.com>
2024-01-03 13:36:36 +0100

    feat: added alertTest and PHPUnit

---
+++ tests/src/AlertTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace src;
+
+include(realpath(__DIR__ . '/../../') . '/GymMel_Alumni/autoload.php');
+
+use PHPUnit\Framework\TestCase;
+use src\Alert;
+
+class AlertTest extends TestCase {
+    public function testDangerAlert()
+    {
+        $alert = new Alert();
+        $message = "This is a danger alert";
+        $expected = '<div class="alert alert-danger" role="alert">This is a danger alert</div>';
+        $this->assertEquals($expected, $alert->dangerAlert($message));
+    }
+
+    public function testSuccessAlert()
+    {
+        $alert = new Alert();
+        $message = "This is a success alert";
+        $expected = '<div class="alert alert-success" role="alert">This is a success alert</div>';
+        $this->assertEquals($expected, $alert->successAlert($message));
+    }
+}
+
