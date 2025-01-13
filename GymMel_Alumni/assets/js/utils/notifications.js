fe0fad923c0691326d754947c6ec4467b5cc2841
Jan <janharms07@gmx.de>
2025-01-13 19:36:30 +0100

    fix: change copyright year

--- GymMel_Alumni/assets/js/utils/notifications.js
+++ GymMel_Alumni/assets/js/utils/notifications.js
@@ -7,7 +7,7 @@
  *
  * @package   GymMel_Alumni
  * @author    Jan Harms <model_railroader@gmx-topmail.de>
- * @copyright 2023-2024 Gymnasium Melle
+ * @copyright 2023-2025 Gymnasium Melle
  * @license   https://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
  * @since     2024-02-24
  */
